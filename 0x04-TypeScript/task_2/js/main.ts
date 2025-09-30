9c6eb95cd96df48615c80c95cd32187f6306d578
Kawona <ojekaona@gmail.com>
2025-09-30 22:14:02 +0000

    Updated function createEmployee in main.ts file

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -69,11 +69,16 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
-    return new Teacher();
+  if (typeof salary === "number") {
+    if (salary < 500) {
+      return new Teacher();
+    } else {
+      return new Director();
+    }
   }
   return new Director();
 }
+
 
 class studentClass implements studentClassInterface {
   firstName: string;
